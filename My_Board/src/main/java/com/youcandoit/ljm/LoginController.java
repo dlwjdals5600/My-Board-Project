3f1cd86445e14677441ef8a5c6147a8bc5f4b3b5
이정민 <dlwjdals5600@gmail.com>
2023-11-15 11:06:06 +0900

    Oracle DBConnection Test 확인

--- My_Board/src/main/java/com/youcandoit/ljm/LoginController.java
+++ My_Board/src/main/java/com/youcandoit/ljm/LoginController.java
@@ -3,7 +3,9 @@ package com.youcandoit.ljm;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +21,17 @@ public class LoginController {
 		return "loginForm";
 	}
 	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션을 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+	public String login(String id, String pwd, String toURL, boolean rememberId,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// 1. id와 pwd를 확인
 		if(!loginCheck(id, pwd)) {
 			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "uft-8");
@@ -29,7 +40,10 @@ public class LoginController {
 			return "redirect:/login/login?msg="+msg;
 		}
 		// 2-2. id와 pwd가 일치하면,
-		// 아이디 기억 체크 유무 확인
+		// 세션 객체를 얻어오기
+		HttpSession session = request.getSession();
+		// 세션 객체에 id를 저장
+		session.setAttribute("id",id);
 		
 		if(rememberId) {
 //			1. 쿠키를 생성
@@ -46,7 +60,9 @@ public class LoginController {
 		}
 
 //				3. 홈으로 이동
-		return "redirect:/";
+		toURL = toURL==null || toURL.equals("") ? "/" : toURL;
+		
+		return "redirect:"+toURL;
 	}
 
 	private boolean loginCheck(String id, String pwd) {
--- My_Board/src/main/webapp/WEB-INF/views/boardList.jsp
+++ My_Board/src/main/webapp/WEB-INF/views/boardList.jsp
@@ -1,15 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
 <c:set var="LoginOutLink" value="${sessionScope.id==null ? '/login/login' : '/login/logout'}"/>
 <c:set var="LoginOut" value="${sessionScope.id==null ? 'Login' : 'Logout'}"/>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
     <title>you can do it</title>
     <link rel="stylesheet" href="<c:url value='/css/menu.css'/>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>    
 </head>
 <body>
 <div id="menu">
@@ -17,13 +16,16 @@
 	    <li id="logo">you can do it</li>
 	    <li><a href="<c:url value='/'/>">Home</a></li>
 	    <li><a href="<c:url value='/board/list'/>">Board</a></li>
-	    <li><a href="<c:url value='${LoginOutLink}'/>">${LoginOut}</a></li>    
+	    <li><a href="<c:url value='${LoginOutLink}'/>">${LoginOut}</a></li>     
 	    <li><a href="<c:url value='/register/add'/>">Sign in</a></li>
 	    <li><a href=""><i class="fas fa-search small"></i></a></li>
 	</ul> 
+</div><div style="text-align:center">
+	<h1>This is BOARD</h1>
+	<h1>This is BOARD</h1>
+	<h1>This is BOARD</h1>
+	<h1>This is BOARD</h1>
+	<h1>This is BOARD</h1>
 </div>
-<div style="text-align:center">
-	<h1>This is HOME</h1>
-	<h1>This is HOME</h1>
-	<h1>This is HOME</h1>
-</div>
+</body>
+</html>
